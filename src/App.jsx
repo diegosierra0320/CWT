b70fae010e8499b551c3aca3241f4a651e622dd2
diegosierra0320 <diego.sierra0320@gmail.com>
2022-09-16 18:36:04 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className='header'>
+        <img src="/utils/CWT+logo white.png" alt="" />
+      </header>
+      <div className='contactos'>
+        <h3>CONTACTOS</h3>
+      </div>
+      <div className='herramientas'>
+        <h3>LINKS HERRAMIENTAS CWT</h3>
+      </div>
+      <div className='aerolineas'>
+        <h3>LINKS AEROLINEAS</h3>
+      </div>
+      <div className='info1'>
+      <article className='card'>
+            <ul className='card__list'>
+              <div  className='card__item'>
+                <li><b>Trámites Migratorios</b><br /><span>tramitesmigratorios@mycwt.com</span></li>
+                <i className="fa-solid fa-passport"></i>
+              </div> <hr />
+              <div className='card__item'>
+                <li><b>Power Library</b><br /><span>ActualizacionesPL@mycwt.com</span></li>
+                <i className="fa-solid fa-book"></i>
+              </div> <hr />
+              <div className='card__item'>
+                <li><b>Power Hotel</b><br /><span>requestsphlatam@mycwt.com</span></li>
+                <i className="fa-solid fa-hotel"></i>
+              </div> <hr />
+              <div className='card__item'>
+                <li><b>Verint</b><br /><span>WFMSpanish@mycwt.com</span></li>
+                <i className="fa-solid fa-clock"></i>
+              </div>
+            </ul>
+          </article>
+      </div>
+      <div className='info2'>
+          <a className='p-hotel' href="https://int.cwthotel360.com/hotelsearch.aspx?params=zp4oX5hkHY2pq9uBKtB4Qeech3U5qlmb1c+ioyhAM0rMnJM8iHgBdvNFEWIeGiM5E6DbYNaXMq9+SD/YxfsJzhy6894okZsKPUKtFO5jfTw=" target="_blank">
+            <img src="https://int.cwthotel360.com/images/Logo.png" alt="" />
+            <b>Power Hotel</b> 
+          </a>
+        <a className='p-library' href="https://iprod1powerlibrary.mycwt.com/" target={"_blank"}>
+          <img src="https://iprod1powerlibrary.mycwt.com/Images/power-library-text.png" alt="" />
+          <b>Power Library</b>
+        </a>
+        <a className='portrait' href="https://maint.mycwt.com/" target={"_blank"}>
+          <img src="/utils/CWT.png" alt="" />
+          <b>Portrait</b>
+        </a>
+        <a className='p-amadeus' href="https://1a.sellingplatformconnect.amadeus.com/LoginService/login.jsp?SITE=LOGINURL&LANGUAGE=GB" target={"_blank"}>
+          <img src="https://img2.freepng.es/20180612/ohx/kisspng-amadeus-it-group-amadeus-crs-computer-reservation-abacus-group-5b204cca84db40.0752248415288434665442.jpg" alt="" />
+          <b>Amadeus</b>
+        </a>
+        <a className='sto' href="https://www.cwtsmarttraveller.com.br/cwtco/default.aspx" target={"_blank"}>
+          <img src="https://static-us.useargo.com//images/logo_argo.png?v=26/02/2020%2011:57:13%20a.%20m." alt="" />
+          <b>STO</b>
+        </a>
+        <a className='verint' href="https://wfo.mt2.verintcloudservices.com/wfo/control/signin" target={"_blank"}>
+          <img src="https://wfo.mt2.verintcloudservices.com/wfo/branding/default/main_two_step.png?v=15.2.924.697" alt="" />
+          <b>Verint</b>
+        </a>
+      </div>
+      <div className='info3'>
+        <a className='ags-360' href="https://ags360.csavianca.com/User/Login?ReturnUrl=%2fAgencySupport%2fIndexAgency#no-back-button" target={"_blank"}>
+          <img src="https://ags360.csavianca.com/img/rojo-listo.gif" alt="" />
+          <b>AGS 360-AV</b>
+        </a>
+        <a className='latam-trade' href="https://www.latamtrade.com/" target={"_blank"}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Latam-logo_-v_%28Indigo%29.svg/1200px-Latam-logo_-v_%28Indigo%29.svg.png" alt="" />
+          <b>LATAM TRADE</b>
+        </a>
+        <a className='ctm' href="https://www.ctmenlinea.com.co/es" target={"_blank"}>
+          <img src="https://cdn.bmpcloud.com/static/assets/business/ctm/images/logo.png?v=20155" alt="" />
+          <b>CTM</b>
+        </a>
+        <a className='jet-smart' href="https://www.jetsmart.com/" target={"_blank"}>
+          <img src="https://www.jetsmart.com/images/header/jetsmart-logo-colored.svg" alt="" />
+          <b>JET SMART</b>
+        </a>
+        <a className='volaris' href="https://www.volaris.com/" target={"_blank"}>
+          <img src="https://www.volaris.com/uipl-assets/images/volaris-logo.svg" alt="" />
+          <b>VOLARIS</b>
+        </a>
+        <a className='american' href="https://saleslink.aa.com/en-US/index.html#/home" target={"_blank"}>
+          <img src="https://www.aa.com/content/images/chrome/rebrand/aa-logo.png" alt="" />
+          <b>AMERICAN</b>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
