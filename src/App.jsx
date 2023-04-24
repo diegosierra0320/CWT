10794ec4d99bb6e437103ad427a3c436999eb463
Sierra <UDES074@mycwt.com>
2023-04-24 18:26:32 -0500

    preferencial supplier card modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 
 function App() {
 
+  // const [openModal, setOpenModal] = useState(false)
+
+  // const handleOpenModal = () => {
+  //   setOpenModal(true)
+  // }
+
+  // const handleCloseModal = () => {
+  //   setOpenModal(false)
+  // }
+
 
   return (
     <div className='App'>
+
+      {/* <button className='openModal' onClick={handleOpenModal}>Open modal</button>
+
+      <div className={openModal ? 'card-container' : 'modal-none'}>
+        <div className='card-example'>
+        <button onClick={handleCloseModal} className='card-example_close'>x</button>
+          <img src="https://1000marcas.net/wp-content/uploads/2020/11/United-Airlines-logo-768x415.png" alt="" />
+          <ul>
+            <h3>Beneficios</h3>
+            <li>Exoneraciones de penalidad</li>
+            <li>Waivers</li>
+            <li>Upgrades</li>
+          </ul>
+          <a href="https://jetstream.united.com/?lang=es#/public-landing" target={'_blank'}>
+          <button className='card-example_go' onClick={handleCloseModal}>Ir</button>
+            </a>
+        </div>
+      </div> */}
+
       <div>
         <Card />
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,196 +8,224 @@ const Card = () => {
       type: "CWT",
       name: "BUZZ",
       url: "https://mycwt.jiveon.com/",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
     {
       id: 2,
       type: "CWT",
       name: "Power Library",
       url: "https://iprod1powerlibrary.mycwt.com/",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
     {
       id: 3,
       type: "CWT",
       name: "Power Hotel",
       url: "https://int.cwthotel360.com/hotelsearch.aspx?params=zp4oX5hkHY2pq9uBKtB4Qeech3U5qlmb1c+ioyhAM0rMnJM8iHgBdvNFEWIeGiM5E6DbYNaXMq9+SD/YxfsJzhy6894okZsKPUKtFO5jfTw=",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
     {
       id: 4,
       type: "CWT",
       name: "Portrait",
       url: "https://maint.mycwt.com/",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
     {
       id: 5,
       type: "CWT",
       name: "Amadeus",
       url: "https://1a.sellingplatformconnect.amadeus.com/LoginService/login.jsp?SITE=LOGINURL&LANGUAGE=GB",
-      img: "https://img2.freepng.es/20180612/ohx/kisspng-amadeus-it-group-amadeus-crs-computer-reservation-abacus-group-5b204cca84db40.0752248415288434665442.jpg"
+      img: "https://img2.freepng.es/20180612/ohx/kisspng-amadeus-it-group-amadeus-crs-computer-reservation-abacus-group-5b204cca84db40.0752248415288434665442.jpg",
+      preferencial: "no"
     },
     {
       id: 6,
       type: "CWT",
       name: "STO",
       url: "https://www.cwtsmarttraveller.com.br/cwtco/default.aspx",
-      img: "https://static-us.useargo.com//images/logo_argo.png?v=26/02/2020%2011:57:13%20a.%20m."
+      img: "https://static-us.useargo.com//images/logo_argo.png?v=26/02/2020%2011:57:13%20a.%20m.",
+      preferencial: "no"
     },
     {
       id: 7,
       type: "CWT",
       name: "Verint",
       url: "https://wfo.mt2.verintcloudservices.com/wfo/control/signin",
-      img: "https://wfo.mt2.verintcloudservices.com/wfo/branding/default/main_two_step.png?v=15.2.924.697"
+      img: "https://wfo.mt2.verintcloudservices.com/wfo/branding/default/main_two_step.png?v=15.2.924.697",
+      preferencial: "no"
     },
     {
       id: 8,
       type: "CWT",
       name: "Power Advantage",
       url: "https://poweradvantage.mycwt.com/login-page",
-      img: "https://mycwt.jiveon.com/groups/image/1491/2.png?a=46547"
+      img: "https://mycwt.jiveon.com/groups/image/1491/2.png?a=46547",
+      preferencial: "no"
     },
     {
       id: 9,
       type: "CWT",
       name: "CXP",
       url: "https://cwt-cwtcxp1-prod1.pegacloud.net/prweb/PRAuth/app/CXP_/5NwfVO74dl9eO-R22P3BMnEyYibt8_7V*/!STANDARD",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
     {
       id: 10,
       type: "CWT",
       name: "CWT Password",
       url: "https://cwt.identitynow.com/login/login/?goto=https:%2F%2Fcwt.login.sailpoint.com%2Foauth%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3D8hq2mbZ5Hrg6TVEz%26redirect_uri%3Dhttps:%2F%2Fcwt.identitynow.com%2Foauth%2Fcallback%26state%3DLrqvOduQsLffvmH5QBigVfFU5QrkCK4wHNGEQoxNdfFvvTfgkiMPV0vLQ6DaugnEKHjrjBYQdj1Enrxr0JvDuAGQAAEzzODAtrRa7gMIZR7RJZVQNxRzvmClyy71bbWvw96TB9cjWFRxeE5Jp7HHXYcd7AIeMwwkSkpAexusSEKs7z7cIIV64zabXvX1DA9B0HiKUiE7geQ97wCb9xstRkfBTlNClkKRLX7gckSM2tq3SGMr88FGPESFAfX3zRCa%26authId%3D5c7fe3e5-608f-4990-ba0b-c496c2ac42a4",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
     {
       id: 11,
       type: "suppliers",
       name: "Puntos Latam",
       url: "https://puntoslatam.com.co/autenticacion",
-      img: "https://megaincentivos-p.s3.amazonaws.com/static/images/Latam/logo_image.png"
+      img: "https://megaincentivos-p.s3.amazonaws.com/static/images/Latam/logo_image.png",
+      preferencial: "yes"
     },
     {
       id: 12,
       type: "suppliers",
-      name: "AGS 360",
-      url: "https://ags360.csavianca.com/User/Login?ReturnUrl=%2fAgencySupport%2fIndexAgency#no-back-button",
-      img: "https://ags360.csavianca.com/img/rojo-listo.gif"
+      name: "Latam trade",
+      url: "https://www.latamtrade.com/",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Latam-logo_-v_%28Indigo%29.svg/1200px-Latam-logo_-v_%28Indigo%29.svg.png",
+      preferencial: "yes"
     },
     {
       id: 13,
       type: "suppliers",
-      name: "Avianca trade",
-      url: "https://www.avianca.com/co/es/trade/",
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcDKSzXuHGZeVUCYqdP_WFCmYkfjcMgmdmGoJBhrpxWBoAPBzDkqBjwY5Hv9285bfeLxw&usqp=CAU"
+      name: "Delta PRO",
+      url: "https://pro.delta.com/content/agency/erp/es/login.html#",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Delta_logo.svg/368px-Delta_logo.svg.png",
+      preferencial: "yes"
     },
     {
       id: 14,
       type: "suppliers",
-      name: "Latam trade",
-      url: "https://www.latamtrade.com/",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Latam-logo_-v_%28Indigo%29.svg/1200px-Latam-logo_-v_%28Indigo%29.svg.png"
+      name: "United",
+      url: "https://jetstream.united.com/?lang=es#/public-landing",
+      img: "https://1000marcas.net/wp-content/uploads/2020/11/United-Airlines-logo-768x415.png",
+      preferencial: "yes"
     },
     {
       id: 15,
       type: "suppliers",
-      name: "CTM",
-      url: "https://www.ctmenlinea.com.co/es",
-      img: "https://cdn.bmpcloud.com/static/assets/business/ctm/images/logo.png?v=20155"
+      name: "American",
+      url: "https://ssc.aa.com/siebel/app/prmportal/enu/?SWECmd=GotoView&SWEView=Home+Page+View+(SCW)&SWERF=1&SWEHo=&SWEBU=1&SWEApplet0=Login+Applet+(PRM)&SWERowId0=VRId-0",
+      img: "https://www.aa.com/content/images/chrome/rebrand/aa-logo.png",
+      preferencial: "yes"
     },
     {
       id: 16,
       type: "suppliers",
-      name: "Jet smart",
-      url: "https://www.jetsmart.com/",
-      img: "https://www.jetsmart.com/images/header/jetsmart-logo-colored.svg"
+      name: "AGS 360",
+      url: "https://ags360.csavianca.com/User/Login?ReturnUrl=%2fAgencySupport%2fIndexAgency#no-back-button",
+      img: "https://ags360.csavianca.com/img/rojo-listo.gif",
+      preferencial: "no"
     },
     {
       id: 17,
       type: "suppliers",
-      name: "Volaris",
-      url: "https://www.volaris.com/",
-      img: "https://www.volaris.com/uipl-assets/images/volaris-logo.svg"
+      name: "Avianca trade",
+      url: "https://www.avianca.com/co/es/trade/",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcDKSzXuHGZeVUCYqdP_WFCmYkfjcMgmdmGoJBhrpxWBoAPBzDkqBjwY5Hv9285bfeLxw&usqp=CAU",
+      preferencial: "no"
     },
     {
       id: 18,
       type: "suppliers",
-      name: "American",
-      url: "https://ssc.aa.com/siebel/app/prmportal/enu/?SWECmd=GotoView&SWEView=Home+Page+View+(SCW)&SWERF=1&SWEHo=&SWEBU=1&SWEApplet0=Login+Applet+(PRM)&SWERowId0=VRId-0",
-      img: "https://www.aa.com/content/images/chrome/rebrand/aa-logo.png"
+      name: "CTM",
+      url: "https://www.ctmenlinea.com.co/es",
+      img: "https://cdn.bmpcloud.com/static/assets/business/ctm/images/logo.png?v=20155",
+      preferencial: "no"
     },
     {
       id: 19,
       type: "suppliers",
-      name: "Viva aerobus",
-      url: "https://web.vivaaerobus.com/mx/vivaempresas",
-      img: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Vivaaerobus-logo.png"
+      name: "Jet smart",
+      url: "https://www.jetsmart.com/",
+      img: "https://www.jetsmart.com/images/header/jetsmart-logo-colored.svg",
+      preferencial: "no"
     },
     {
       id: 20,
       type: "suppliers",
-      name: "Viva Air",
-      url: "https://www.vivaair.com/co/es/user/profile",
-      img: "./utils/viva-air.svg"
+      name: "Volaris",
+      url: "https://www.volaris.com/",
+      img: "https://www.volaris.com/uipl-assets/images/volaris-logo.svg",
+      preferencial: "no"
     },
     {
       id: 21,
       type: "suppliers",
-      name: "Plataforma 10",
-      url: "https://red.plataforma10.net/?returnUrl=%2FPVs%2FNotificaciones&sesionExpirada=True",
-      img: "https://red.plataforma10.net/public/images/logo-plataforma10-login.jpg"
+      name: "Viva aerobus",
+      url: "https://web.vivaaerobus.com/mx/vivaempresas",
+      img: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Vivaaerobus-logo.png",
+      preferencial: "no"
     },
     {
       id: 22,
       type: "suppliers",
-      name: "Central de pasajes",
-      url: "https://www.aiv.tur.ar/aiv/ingreso.htm",
-      img: "https://www.centraldepasajes.com.ar/cdp/img/home/logo-central-de-pasajes.png"
+      name: "Viva Air",
+      url: "https://www.vivaair.com/co/es/user/profile",
+      img: "./utils/viva-air.svg",
+      preferencial: "no"
     },
     {
       id: 23,
       type: "suppliers",
-      name: "Buquebus",
-      url: "https://agencias.buquebus.com/",
-      img: "/utils/Buque.svg"
+      name: "Plataforma 10",
+      url: "https://red.plataforma10.net/?returnUrl=%2FPVs%2FNotificaciones&sesionExpirada=True",
+      img: "https://red.plataforma10.net/public/images/logo-plataforma10-login.jpg",
+      preferencial: "no"
     },
     {
       id: 24,
       type: "suppliers",
-      name: "Delta PRO",
-      url: "https://pro.delta.com/content/agency/erp/es/login.html#",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Delta_logo.svg/368px-Delta_logo.svg.png"
+      name: "Central de pasajes",
+      url: "https://www.aiv.tur.ar/aiv/ingreso.htm",
+      img: "https://www.centraldepasajes.com.ar/cdp/img/home/logo-central-de-pasajes.png",
+      preferencial: "no"
     },
     {
       id: 25,
       type: "suppliers",
-      name: "United",
-      url: "https://jetstream.united.com/?lang=es#/public-landing",
-      img: "https://1000marcas.net/wp-content/uploads/2020/11/United-Airlines-logo-768x415.png"
+      name: "Buquebus",
+      url: "https://agencias.buquebus.com/",
+      img: "/utils/Buque.svg",
+      preferencial: "no"
     },
     {
       id: 26,
       type: "suppliers",
       name: "KIU Click",
       url: "https://kiu.click/login/",
-      img: "https://aerolatinnews.com/wp-content/uploads/2014/08/KIU_MASTER_CHICO_CMYK-e1415184223322.jpg"
+      img: "https://aerolatinnews.com/wp-content/uploads/2014/08/KIU_MASTER_CHICO_CMYK-e1415184223322.jpg",
+      preferencial: "no"
     },
     {
       id: 27,
       type: "suppliers",
       name: "Hahn Air",
       url: "https://www.hahnair.com/es",
-      img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Hahn_Air_Logo.svg/263px-Hahn_Air_Logo.svg.png"
+      img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Hahn_Air_Logo.svg/263px-Hahn_Air_Logo.svg.png",
+      preferencial: "no"
     },
     {
       id: 28,
       type: "CWT",
       name: "Penalidades Aerolineas",
       url: "https://mycwt.jiveon.com/docs/DOC-29232",
-      img: "/utils/CWT.png"
+      img: "/utils/CWT.png",
+      preferencial: "no"
     },
   ])
 
@@ -296,7 +324,7 @@ const Card = () => {
         <div className='content-1_container'>
           {
             searchResult.filter(item => item.type === "CWT").map(item => (
-                <div className='card' key={item.id}>
+                <div className="card" key={item.id}>
                   <a href={item.url} target="_blank">
                     <div>
                       <h2 className='card_name'>
@@ -318,7 +346,7 @@ const Card = () => {
         <div className='content-2_container'>
           {
             searchResult.filter(item => item.type === "suppliers").map(item => (
-                <div className='card' key={item.id}>
+                <div className={`card border-${item.preferencial}`} key={item.id}>
                   <a href={item.url} target="_blank">
                     <div>
                       <h2 className='card_name'>
